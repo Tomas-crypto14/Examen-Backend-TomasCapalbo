f3a5b6433651442ccfec0655b8f94e6df406cdf2
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2025-04-30 10:25:01 +0200

    Examen Backend

---
+++ Servidor/index.js
@@ -0,0 +1,73 @@
+const port = 8000;
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//EJERCICIO 1
+let comida = [
+    {
+        id: 1,
+        comida: "Hamburguesa"
+    },
+    {
+        id: 2,
+        comida: "Pizza"
+    },
+    {
+        id: 3,
+        comida: "Macarrones"
+    }
+]
+
+//let comida = ["Hamburguesa", "Pizza", "Macarrones"];
+
+let randomFood = Math.floor(Math.random()*comida.length);
+
+app.get("/comida", (req, res) => {
+    res.send(comida[randomFood]);
+})
+
+//EJERCICIO 2
+let minmax = [
+    
+]
+app.post("/minmax", (req, res) => {
+    //const minMath = Math.min(minmax);
+    //const maxMath = Math.max(minmax);
+    console.log(req.body.number);
+    minmax.push({
+        min: req.body.number,
+        max: req.body.number
+    })
+    res.send("Numero introducido");
+})
+app.get("/minmax", (req, res) => {
+    res.send(minmax)
+})
+
+//EJERCICIO 3
+//Estaba puesto el app.put en vez de app.delete, put es para actualizar y eso es un codigo para eliminar usuarios,
+//no para actualizarlos.
+app.delete('/users', (res, req) => {
+    const userId = req.params.id;
+    const sql = `DELETE FROM users WHERE id=${userID}`;
+    db.query(sql, (error, result) => {
+        if(error) throw error;
+        res.send(`User ${userId} deleted from the db.`);
+    })
+})
+
+/*const db = mysql.createConnection({
+    host    : 'localhost',
+    user    : 'root',
+    password: 'root',
+    database: 'ExamennodeJS',
+})
+db.connect()*/
+app.listen(port, () => {
+    console.log("Puerto escuchado, que empiece el examen!")
+})
